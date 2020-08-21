021a8e378bf23a44ed4ed724d5f5f1831afc9513
Jorge Casariego <jorgekasa@gmail.com>
2020-08-21 16:59:17 -0400

    Make a ServiceLocator and launch a Fragment from a Test using the ServiceLocator created

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,57 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import FakeAndroidTestRepository
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@MediumTest
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class TaskDetailFragmentTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun cleanUpDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun activeTasktDetails_DisplayedIntUi() = runBlockingTest {
+        // GIVEN - Add active (incomplete) task to the DB
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
+        repository.saveTask(activeTask)
+
+        // WHEN - Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+
+        /**
+         * Note: Supplying the theme is necessary because fragments usually get their theming from
+         * their parent activity. When using FragmentScenario, your fragment is launched inside a
+         * generic empty activity so that it's properly isolated from activity code (you are just
+         * testing the fragment code, not the associated activity). The theme parameter allows you
+         * to supply the correct theme.
+         */
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+    }
+
+}
